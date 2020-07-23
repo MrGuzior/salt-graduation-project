12b6eee135e4ab54b689c28c3dac841ff0fd3161
MrGuzior <konrad.guzior@gmail.com>
2020-07-23 08:53:35 +0200

    Added new schema

--- server/app.js
+++ server/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const cors = require("cors");
+const MongoClient = require('mongodb');
+const fs = require('fs');
 
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express();
+
+const data = JSON.parse(fs.readFileSync('./data/data.json', 'utf-8'));
+//console.log(data.data.users[0].waste_history)
+
+// const dbURL = 'mongodb://localhost:27017';
+// const dbName = 'Wastr';
 
 app.use(cors());
 app.use(logger('dev'));
@@ -16,23 +24,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// const insertDocuments = function (db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('Wastr');
+//     // Insert some documents
+//     collection.insertMany([
+//         { a: 1 }, { a: 2 }, { a: 3 }
+//     ], function (err, result) {
+//         assert.equal(err, null);
+//         assert.equal(3, result.result.n);
+//         assert.equal(3, result.ops.length);
+//         console.log("Inserted 3 documents into the collection");
+//         callback(result);
+//     });
+// }
+
+// MongoClient.connect(dbURL, function (err, client) {
+//     console.log("Connected successfully to server");
+
+//     const db = client.db(dbName);
+
+//     insertDocuments(db, function () {
+//         client.close();
+//     });
+// });
+
+
 
 const schema = buildSchema(`
     type Query {
-        hello: String,
-        goodbye: String
+        users: [User],
+        user(id: String): User
+    }
+
+    type User {
+        name: String,
+        id: String,
+        waste_history: [Waste]
+    }
+
+    type Waste {
+        date: String,
+        amount: String,
+        type: String
     }
 `);
 
 const root = {
-    hello: () => {
-        return 'Hello world!';
+    users: () => {
+        return data.data.users;
     },
-    goodbye: () => {
-        return 'Goodbye world!'
-    }
+    user: (args) => {
+        return data.data.users.find(user => user.id == args.id);
+    },
+    id: () => {
+        return data.data.users[0].id;
+    },
 };
-
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
